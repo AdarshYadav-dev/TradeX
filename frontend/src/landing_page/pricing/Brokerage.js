221c9292521bbbf204514bbef2fc8d36d3b8dbf2
AdarshYadav-dev <adarsh98215@gmail.com>
2025-12-28 12:05:07 +0530

    working-done on boilder plate code in landing page

---
+++ frontend/src/landing_page/pricing/Brokerage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Brokerage() {
+    return (
+        <div className="brokerage-section">
+            <h2>Brokerage</h2>
+        </div>
+    )
+}
+
+export default Brokerage
---
+++ frontend/src/landing_page/products/Universe.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Universe() {
+  return (
+    <div className="universe-section">
+      <h2>Universe</h2>
+    </div>
+  );
+}
+
+export default Universe;
