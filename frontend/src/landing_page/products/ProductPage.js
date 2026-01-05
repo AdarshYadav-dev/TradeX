d1671babf5df51d37eb11bca97b7da3b4759f123
AdarshYadav-dev <adarsh98215@gmail.com>
2026-01-05 23:30:31 +0530

    Add react-router-dom and configure page routing

---
+++ frontend/src/landing_page/products/ProductPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../home/Hero'
+import LeftSection from './LeftSection'
+import RightSection from './RightSection'
+import Universe from './Universe'
+
+const PricingPage = () => {
+    return (
+        <>
+            <Hero />
+            <LeftSection />
+            <RightSection />
+            <Universe />
+        </>
+    )
+}
+
+export default PricingPage
